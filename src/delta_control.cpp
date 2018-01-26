8ccf0fa5538e080af665e3a8b3aa2247414bec5c
Miquel Puig <mpuig22@gmail.com>
2018-01-26 01:46:00 +0100

    delta control

---
+++ src/delta_control.cpp
@@ -0,0 +1,99 @@
+// robot geometry
+//(look at pics above for explanation)
+
+#include "delta_node.h"
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "geometry_msgs/Vector3.h"
+#include <sstream>
+#include "std_msgs/UInt16MultiArray.h"
+#include <vector>
+#include <cmath>
+
+
+
+
+cv::Mat inPosition=(cv::Mat_<double>(3,1)<< 0,0,0);
+
+//Fixar posició inicial que correspongui als angles de delta_kinematics
+cv::Mat outPosition=(cv::Mat_<double>(3,1)<< 0,0,0);
+
+ void chatterCallback(const geometry_msgs::Vector3& vector)
+ {
+   inPosition=(cv::Mat_<double>(3,1)<< vector.x,vector.y,vector.z);
+
+ }
+
+int main(int argc, char **argv)
+{
+
+  int rate =5; //Hz
+  int totaltime=4; //segons
+  int prod=rate*totaltime;
+
+  ros::init(argc, argv, "delta_control");
+
+  ros::NodeHandle n;
+
+  ros::Subscriber sub = n.subscribe("end_position", 10,chatterCallback);
+
+  ros::Publisher chatter_pub = n.advertise<geometry_msgs::Vector3>("position", 1);
+
+
+  ros::Rate loop_rate(rate);
+
+  int count = 0;
+
+
+  double xIncfix=(inPosition.at<double>(0,0)- outPosition.at<double>(0,0))/prod;
+  double yIncfix=(inPosition.at<double>(1,0)- outPosition.at<double>(1,0))/prod;
+  double zIncfix=(inPosition.at<double>(2,0)- outPosition.at<double>(2,0))/prod;
+
+  while (ros::ok())
+  {
+
+    if(count<prod){
+
+      double xInc=inPosition.at<double>(0,0)- outPosition.at<double>(0,0);
+      double yInc=inPosition.at<double>(1,0)- outPosition.at<double>(1,0);
+      double zInc=inPosition.at<double>(2,0)- outPosition.at<double>(2,0);
+
+      if(xInc>0.5 || xInc<0.5){
+        outPosition.at<double>(0,0) = outPosition.at<double>(0,0)+xIncfix;
+      }
+      if(yInc>0.5 || yInc<0.5){
+        outPosition.at<double>(1,0) = outPosition.at<double>(1,0)+yIncfix;
+      }
+      if(yInc>0.5 || zInc<0.5){
+        outPosition.at<double>(2,0) = outPosition.at<double>(2,0)+zIncfix;
+      }
+
+      count+=1;
+      //ROS_INFO("midxIncfix: [%lf]",xIncfix );
+  }else{
+    //ROS_INFO("####Paso por aqui: [%lf]",xIncfix);
+        count=0;
+    //ROS_INFO("finalxIncfix: [%lf]",xIncfix );
+
+    //Refixar objectiu
+     xIncfix=(inPosition.at<double>(0,0)- outPosition.at<double>(0,0))/prod;
+     yIncfix=(inPosition.at<double>(1,0)- outPosition.at<double>(1,0))/prod;
+     zIncfix=(inPosition.at<double>(2,0)- outPosition.at<double>(2,0))/prod;
+    //ROS_INFO("inicialxIncfix: [%lf]",xIncfix );
+  }
+
+
+    geometry_msgs::Vector3 msg;
+    msg.x = outPosition.at<double>(0, 0);
+    msg.y = outPosition.at<double>(1, 0);
+    msg.z = outPosition.at<double>(2, 0);
+    chatter_pub.publish(msg);
+
+    //ROS_INFO("%s", msg.data.c_str());
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+
+  }
+}
--- src/delta_kinematics.cpp
+++ src/delta_kinematics.cpp
@@ -1,14 +1,21 @@
 // robot geometry
 //(look at pics above for explanation)
-#include <cmath>
+
 #include "delta_node.h"
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "geometry_msgs/Vector3.h"
+#include <sstream>
+#include "std_msgs/UInt16MultiArray.h"
+#include <vector>
+#include <cmath>
 
 const float e = 110;     // Longitud del lado del triángulo equilátero pequeño (el que se moverá).
 const float f = 300;     //MODIFICARRRRR Longitud del lado del triángulo equilátero grande  (el que estará estático).
 const float re = 240;    // Longitud del brazo que va unido al triángulo pequeño y a 'rf'.
 const float rf = 150;    // Longitud del pequeño brazo que está unido al eje del servo y a 're'.
 
-// trigonometric constants
+
 const float sqrt3 = sqrt(3.0);
 const float pi = 3.141592653;    // Pi
 const float sin120 = sqrt3/2.0;
@@ -21,8 +28,7 @@ float theta1;
 float theta2;
 float theta3;
 
-// inverse kinematics
-// helper functions, calculates angle theta1 (for YZ-pane)
+
 int delta_calcAngleYZ(float x0, float y0, float z0, float &theta) {
     float y1 = -0.5 * 0.57735 * f; // f/2 * tg 30
     y0 -= 0.5 * 0.57735    * e;    // shift center to edge
@@ -37,8 +43,7 @@ int delta_calcAngleYZ(float x0, float y0, float z0, float &theta) {
     theta = 180.0*atan(-zj/(y1 - yj))/pi + ((yj>y1)?180.0:0.0); // Si se cumple que yj>y1 entonces suma "180.0", de lo contrario no suma nada "0.0".
     return 0;
 }
-// inverse kinematics: (x0, y0, z0) -> (theta1, theta2, theta3)
-// returned status: 0=OK, -1=non-existing position
+
 int delta_calcInverse(float x0, float y0, float z0, float &theta1, float &theta2, float &theta3) {
     theta1 = theta2 = theta3 = 0;
     int status = delta_calcAngleYZ(x0, y0, z0, theta1);
@@ -47,16 +52,8 @@ int delta_calcInverse(float x0, float y0, float z0, float &theta1, float &theta2
     return status;
 }
 
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "geometry_msgs/Vector3.h"
-#include <sstream>
-#include "std_msgs/UInt16MultiArray.h"
-#include <vector>
+//thetas inicials
 std::vector<int> thetas = { 0, 0 , 0};
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
 
 
  void chatterCallback(const geometry_msgs::Vector3& vector)
@@ -64,24 +61,21 @@ std::vector<int> thetas = { 0, 0 , 0};
 float th1;
 float th2;
 float th3;
-   if(delta_calcInverse(vector.x,vector.y,vector.z,th1,th2,th3)==-1){
-     ROS_INFO("No hay inversa posible");
-   }
-   else{
-  theta1=th1;
-  theta2=th2;
-  theta3=th3;
-   ROS_INFO("theta1: [%f]", theta1);
-   ROS_INFO("theta2: [%f]", theta2);
-   ROS_INFO("theta3: [%f]", theta3);
- }
-   //ROS_INFO("theta: [%f]", vector.x);
-   //ROS_INFO("y: [%f]", vector.y);
-   //ROS_INFO("z: [%f]", vector.z);
+if(vector.x!=0 || vector.y!=0 || vector.z!=0){
 
-
-
- }
+         if(!delta_calcInverse(vector.x,vector.y,vector.z,th1,th2,th3)==0){
+           //ROS_INFO("No hi ha inversa possible");
+         }
+         else{
+        theta1=th1;
+        theta2=th2;
+        theta3=th3;
+         //ROS_INFO("theta1: [%f]", theta1);
+         //ROS_INFO("theta2: [%f]", theta2);
+         //ROS_INFO("theta3: [%f]", theta3);
+       }
+  }
+}
 
 
 int main(int argc, char **argv)
